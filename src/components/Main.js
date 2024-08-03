f7a557290410f77d30f2c821a58e9ddaa759b754
Manh Nam, Tran <manhnambeupu@gmail.com>
2024-08-03 15:23:46 +0200

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -175,7 +175,7 @@ const Main = () => {
                 whileHover={{scale: 1.1}}
                 whileTap={{scale: 0.9}}
                 >
-                    Blog
+                    
                 </motion.h2>
             </BLOG>
             <WORK to="/work" click={+click}>
@@ -191,7 +191,7 @@ const Main = () => {
                  whileHover={{scale: 1.1}}
                 whileTap={{scale: 0.9}}
                 >
-                    Work
+                
                 </motion.h2>
             </WORK>
             <BottomBar>
